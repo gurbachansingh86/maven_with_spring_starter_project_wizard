09aa5575ffb65cba0f527dcefdbe9cd68c4a8346
Gurbachan Singh <gurbachans@valyoo.in>
2018-04-16 02:30:30 +0530

    master add file into this project

---
+++ src/main/java/com/learnspringboot/SpringBootExample.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gurbachan
+ *
+ */
+@SpringBootApplication
+public class SpringBootExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample.class, args);
+	}
+}
